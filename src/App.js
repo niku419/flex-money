6dc3c32504c0ced76842a08340ac450c5b0adb4b
Nikhil Sai Allam <nikhil.logicwind@gmail.com>
2022-12-12 12:43:46 +0530

    :rocket: added basic frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Main from './test/Main.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
